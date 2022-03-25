920d4a29d1c191fe86853d470b5bcba64764c289
ShineiArakawa <82491048+ShineiArakawa@users.noreply.github.com>
2022-03-25 09:43:44 +0900

    update question 21 to 30

--- src/functions.py
+++ src/functions.py
@@ -1,10 +1,15 @@
-from asyncio import windows_events
-from turtle import shape
+# autopep8: off
+import os
+import time
+if os.name == "nt":
+    from asyncio import windows_events
+
 from typing import List, Tuple
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from PIL import Image
+# autopep8: on
 
 
 class Functions:
@@ -152,28 +157,294 @@ class Functions:
     @staticmethod
     def normalizeHistgram(img: np.ndarray, range: Tuple[int]):
         img = img.astype(np.float64)
-        
+
         c = np.min(img)
         d = np.max(img)
-        
+
         output = (range[1]-range[0])*(img-c)/(d-c)+range[0]
-        output[img<c] = range[0]
-        output[img>=d] = range[1]
-        
+        output[img < c] = range[0]
+        output[img >= d] = range[1]
+
         output = np.clip(output, 0, 255)
-        
+
         return output
-    
+
     @staticmethod
     def scaleShiftHistgram(img: np.ndarray, loc: float, scale: float):
         img = img.astype(np.float64)
-        
+
         mean = np.mean(img)
         std = np.std(img)
-        
+
         output = scale * (img - mean) / std + loc
         output = np.clip(output, 0, 255)
-        
+
         return output
-        
-        
+
+    @staticmethod
+    def equalizeHistgram(imgArray: np.ndarray, algorithm: int = 0) -> np.ndarray:
+        """ヒストグラムの平坦化を行うメソッドである
+
+        Args:
+            imgArray (np.ndarray): 入力画像
+            algorithm (int, optional): アルゴリズム番号. Defaults to 0.
+                algorithm=0: グレースケール変化を施してからヒストグラムの平坦化を行う
+                algorithm=1: R,G,Bそれぞれに対してヒストグラムの平坦化を行う
+                algorithm=2: R,G,Bをまとめてヒストグラムの平坦化を行う
+
+        Raises:
+            ValueError: algorithmに不正な値が指定された場合に発生するエラーである
+
+        Returns:
+            np.ndarray: 出力画像
+        """
+
+        output = None
+
+        if algorithm == 0:
+            grayScale = Functions.grayScaleTransform(imgArray)
+            output = np.zeros_like(grayScale)
+
+            x_max = 255  # constant
+            s = 1
+            for dim in grayScale.shape:
+                s *= dim
+            coeff = x_max / s
+
+            hist = np.histogram(grayScale.ravel(), bins=255, range=(0, 255))
+            hist_cumsum = np.cumsum(hist[0])
+
+            output = coeff * hist_cumsum[grayScale.astype(np.uint8)]
+
+        elif algorithm == 1:
+            output = np.zeros_like(imgArray)
+
+            x_max = 255  # constant
+            h, w, c = imgArray.shape
+            s = h * w
+            coeff = x_max / s
+
+            for i in range(c):
+                hist = np.histogram(
+                    imgArray[:, :, i].ravel(), bins=255, range=(0, 255))
+                hist_cumsum = np.cumsum(hist[0])
+
+                output[:, :, i] = coeff * \
+                    hist_cumsum[imgArray[:, :, i].astype(np.uint8)]
+
+        elif algorithm == 2:
+            output = np.zeros_like(imgArray)
+
+            x_max = 255  # constant
+            h, w, c = imgArray.shape
+            s = h * w * c
+            coeff = x_max / s
+
+            hist = np.histogram(imgArray.ravel(), bins=255, range=(0, 255))
+            hist_cumsum = np.cumsum(hist[0])
+
+            output = coeff * hist_cumsum[imgArray.astype(np.uint8)]
+
+        else:
+            raise ValueError("Algorithm ID is invalid!!")
+
+        output = np.clip(output, 0, 255)
+        return output
+
+    @staticmethod
+    def gammaCorrection(imgArray: np.ndarray, c: float, g: float) -> np.ndarray:
+        if imgArray.dtype is not np.float64:
+            imgArray = imgArray.astype(np.float64)
+
+        imgArray_normalized = imgArray / 255
+
+        output = (imgArray_normalized / c)**(1/g) * 255
+
+        output = np.clip(output, 0, 255)
+        return output
+
+    @staticmethod
+    def magnify_nn(imgArray: np.ndarray, magX: float, magY: float) -> np.ndarray:
+        outputHeight = int(imgArray.shape[0] * magX)
+        outputWidth = int(imgArray.shape[1] * magY)
+
+        nChannels = imgArray.shape[2]
+        output = np.zeros(shape=(outputHeight, outputWidth,
+                          nChannels), dtype=np.float64)
+
+        meshX, meshY = np.meshgrid(range(outputHeight), range(outputWidth))
+        output[meshY, meshX] = imgArray[np.round(
+            meshY / magX).astype(int), np.round(meshX / magY).astype(int)]
+
+        return output
+
+    @staticmethod
+    def magnify_biLinear(imgArray: np.ndarray, magX: float, magY: float) -> np.ndarray:
+        # =============================================================
+        # 自作で作成したプログラムであるが、うまく動作しない。
+        # =============================================================
+        # startTime = time.perf_counter()
+
+        # height, width, nChannels = imgArray.shape
+        # outputHeight = int(height * magX)
+        # outputWidth = int(width * magY)
+        # output = np.zeros(shape=(outputHeight, outputWidth,
+        #                   nChannels), dtype=np.float64)
+
+        # for channel in range(nChannels):
+        #     for indexX in range(outputHeight):
+        #         for indexY in range(outputWidth):
+        #             x_origin = indexX/magY
+        #             y_origin = indexY/magX
+
+        #             indexX_origin = np.floor(x_origin).astype(int)
+        #             indexY_origin = np.floor(y_origin).astype(int)
+
+        #             dx = x_origin - indexX_origin
+        #             dy = y_origin - indexY_origin
+
+        #             indexX_origin_plus = np.minimum(
+        #                 indexX_origin + 1, width - 1)
+        #             indexY_origin_plus = np.minimum(
+        #                 indexY_origin + 1, height - 1)
+
+        #             pixel_0_0 = imgArray[indexY_origin][indexX_origin][channel]
+        #             pixel_0_1 = imgArray[indexY_origin][indexX_origin_plus][channel]
+        #             pixel_1_0 = imgArray[indexY_origin_plus][indexX_origin][channel]
+        #             pixel_1_1 = imgArray[indexY_origin_plus][indexX_origin_plus][channel]
+
+        #             output = (1 - dx) * (1 - dy) * pixel_0_0 + dx * (1 - dy) * pixel_0_1 + \
+        #                 (1 - dx) * dy * pixel_1_0 + \
+        #                 dx * dy * pixel_1_1
+
+        # endTime = time.perf_counter()
+        # print(f"elapsedTime= {endTime-startTime} [sec]")
+        # return output
+
+        # =============================================================
+        # ホームページからコピーした実装である。
+        # =============================================================
+        img = imgArray
+        a = magX
+        b = magY
+
+        h, w, c = img.shape
+        out_h = int(h * a)
+        out_w = int(w * b)
+
+        xs, ys = np.meshgrid(range(out_w), range(out_h))  # output image index
+
+        _xs = np.floor(xs / b).astype(int)  # original x
+        _ys = np.floor(ys / a).astype(int)  # original y
+
+        dx = xs / b - _xs
+        dy = ys / a - _ys
+
+        dx = np.repeat(np.expand_dims(dx, axis=-1),
+                       c, axis=-1)  # repeat channel
+        dy = np.repeat(np.expand_dims(dy, axis=-1),
+                       c, axis=-1)  # repeat channel
+
+        _xs1p = np.minimum(_xs + 1, w - 1)
+        _ys1p = np.minimum(_ys + 1, h - 1)
+
+        out = (1 - dx) * (1 - dy) * img[_ys, _xs] + dx * (1 - dy) * img[_ys, _xs1p] + \
+            (1 - dx) * dy * img[_ys1p, _xs] + dx * dy * img[_ys1p, _xs1p]
+
+        return np.clip(out, 0, 255)
+
+    @staticmethod
+    def magnify_biCubic(imgArray: np.ndarray, magX: float, magY: float) -> np.ndarray:
+        startTime = time.perf_counter()
+
+        h, w, c = imgArray.shape
+        out_h = int(h * magX)
+        out_w = int(w * magY)
+        output = np.zeros([out_h, out_w, c], dtype=np.float32)
+
+        xs, ys = np.meshgrid(range(out_w), range(out_h))  # output image index
+
+        _xs = np.floor(xs / magY).astype(int)  # original x
+        _ys = np.floor(ys / magX).astype(int)  # original y
+
+        dx1 = np.abs(xs / magY - (_xs - 1))
+        dx2 = np.abs(xs / magY - _xs)
+        dx3 = np.abs(xs / magY - (_xs + 1))
+        dx4 = np.abs(xs / magY - (_xs + 2))
+        dy1 = np.abs(ys / magX - (_ys - 1))
+        dy2 = np.abs(ys / magX - _ys)
+        dy3 = np.abs(ys / magX - (_ys + 1))
+        dy4 = np.abs(ys / magX - (_ys + 2))
+
+        dxs = [dx1, dx2, dx3, dx4]
+        dys = [dy1, dy2, dy3, dy4]
+
+        def weight(t, a=-1):
+            w = np.zeros_like(t)
+            w[t <= 1] = ((a + 2) * (t ** 3) - (a + 3) * (t ** 2) + 1)[t <= 1]
+            w[t > 1] = (a * (t ** 3) - 5 * a * (t ** 2) +
+                        8 * a * t - 4 * a)[t > 1]
+            return w
+
+        w_sum = np.zeros_like(output, dtype=np.float32)
+
+        for j in range(-1, 3):
+            for i in range(-1, 3):
+                ind_x = np.minimum(np.maximum(_xs + i, 0), w - 1)
+                ind_y = np.minimum(np.maximum(_ys + j, 0), h - 1)
+
+                wx = weight(dxs[i + 1])
+                wy = weight(dys[j + 1])
+                wx = np.repeat(np.expand_dims(wx, axis=-1), 3, axis=-1)
+                wy = np.repeat(np.expand_dims(wy, axis=-1), 3, axis=-1)
+
+                w_sum += wx * wy
+                output += wx * wy * imgArray[ind_y, ind_x]
+
+        output = np.clip(output, 0, 255)
+
+        endTime = time.perf_counter()
+        print(f"elapsedTime= {endTime-startTime} [sec]")
+
+        return output
+
+    @staticmethod
+    def affine(img, affine_mat, out_shape):
+        h, w, channel = img.shape
+
+        [a, b, tx], [c, d, ty] = affine_mat
+        out_h, out_w = out_shape
+
+        out = np.zeros([out_h, out_w, channel])
+
+        # pad for black
+        img_pad = np.pad(img, [(1, 1), (1, 1), (0, 0)])
+
+        xs, ys = np.meshgrid(range(out_w), range(out_h))  # output image index
+
+        _xs = np.maximum(np.minimum(
+            (1 / (a * d - b * c) * (d * xs - b * ys) - tx).astype(int) + 1, w + 1), 0)
+        _ys = np.maximum(np.minimum(
+            (1 / (a * d - b * c) * (- c * xs + a * ys) - ty).astype(int) + 1, h + 1), 0)
+
+        out[ys, xs] = img_pad[_ys, _xs]
+        return np.clip(out, 0, 255).astype(np.uint8)
+
+    @staticmethod
+    def getShiftMat(x: int, y: int, toReturnAll: bool = False):
+        if toReturnAll:
+            return [[1, 0, x],
+                    [0, 1, y],
+                    [0, 0, 1]]
+        return [[1, 0, x],
+                [0, 1, y]]
+
+    @staticmethod
+    def getRotationMat(theta: float, toReturnAll: bool = False):
+        theta = np.deg2rad(theta)
+        if toReturnAll:
+            return [[np.cos(theta), -np.sin(theta), 0],
+                    [np.sin(theta), np.cos(theta), 0],
+                    [0, 0, 1]]
+        return [[np.cos(theta), -np.sin(theta), 0],
+                [np.sin(theta), np.cos(theta), 0]]
--- src/question21to30.py
+++ src/question21to30.py
@@ -20,6 +20,8 @@ class Main:
             "../dataset/images/imori_256x256_noise.png")
         self.img_256_dark = Image.open(
             "../dataset/images/imori_256x256_dark.png")
+        self.img_256_gamma = Image.open(
+            "../dataset/images/imori_256x256_gamma.png")
         # =================================================================================
 
         # Array ===========================================================================
@@ -27,64 +29,134 @@ class Main:
         self.imgArray_256 = np.array(self.img_256, dtype=np.float64)
         self.imgArray_512 = np.array(self.img_512, dtype=np.float64)
 
-        self.imgArray_256_noise = np.array(self.img_256_noise, dtype=np.float64)
+        self.imgArray_256_noise = np.array(
+            self.img_256_noise, dtype=np.float64)
         self.imgArray_256_dark = np.array(self.img_256_dark, dtype=np.float64)
+        self.imgArray_256_gamma = np.array(
+            self.img_256_gamma, dtype=np.float64)
         # =================================================================================
         pass
 
     def question21(self):
         """ヒストグラム正規化
         """
-        imgArray_normalized = Functions.normalizeHistgram(self.imgArray_256_dark, (0, 255))
-        
-        self.__plotImages([self.imgArray_256_dark, imgArray_normalized])
-        
+        imgArray_normalized = Functions.normalizeHistgram(
+            self.imgArray_256_dark, (0, 255))
 
+        self.__plotImages([self.imgArray_256_dark, imgArray_normalized])
 
     def question22(self):
         """スケーリングとシフト
         """
-        imgArray_normalized = Functions.scaleShiftHistgram(self.imgArray_256_dark, 128, 50)
-        
+        imgArray_normalized = Functions.scaleShiftHistgram(
+            self.imgArray_256_dark, 128, 50)
+
         self.__plotImages([self.imgArray_256_dark, imgArray_normalized])
 
-
     def question23(self):
-        """Max-Minフィルタ
+        """ヒストグラム平坦化
         """
+        algorithm = 2
+        imgArray_equalized = Functions.equalizeHistgram(
+            self.imgArray_256_dark, algorithm=algorithm)
 
+        self.__plotImages([self.imgArray_256_dark, imgArray_equalized])
 
     def question24(self):
-        """Differential Filter
+        """ガンマ補正
         """
+        imgArray_gammaCorrected = Functions.gammaCorrection(
+            self.imgArray_256_gamma, c=1, g=2.2)
+        self.__plotImages([self.imgArray_256_gamma, imgArray_gammaCorrected])
 
     def question25(self):
-        """Prewitt Filter
+        """最近傍補間
         """
-
+        imgArray_magnified = Functions.magnify_nn(
+            self.imgArray_256, magX=1.5, magY=1.5)
+        self.__plotImages([self.imgArray_256, imgArray_magnified])
 
     def question26(self):
-        """Sobel Filter
+        """バイリニア補間
         """
-
+        imgArray_magnified = Functions.magnify_biLinear(
+            self.imgArray_256, magX=1.5, magY=2.0)
+        self.__plotImages([self.imgArray_256, imgArray_magnified])
 
     def question27(self):
-        """Laplacian Filter
+        """バイキュービック補間
         """
+        imgArray_magnified = Functions.magnify_biCubic(
+            self.imgArray_256, magX=10.0, magY=10.0)
+        self.__plotImages([self.imgArray_256, imgArray_magnified])
 
     def question28(self):
-        """Emboss Filter
+        """平行移動
         """
-
+        h, w, c = self.imgArray_512.shape
+        imgArray_transformed = Functions.affine(
+            self.imgArray_512, [[1, 0, 30], [0, 1, -30]], (h, w))
+        self.__plotImages([self.imgArray_512, imgArray_transformed])
 
     def question29(self):
-        """LoG filter
+        """拡大縮小
         """
+        magX = 1.3
+        magY = 0.8
+        shiftX = 30
+        shiftY = -30
 
+        h, w, c = self.imgArray_512.shape
+        imgArray_transformed = Functions.affine(self.imgArray_512,
+                                                [[magX, 0, shiftX], [
+                                                    0, magY, shiftY]],
+                                                (int(h*magY), int(w*magX)))
+        self.__plotImages([self.imgArray_512, imgArray_transformed])
 
     def question30(self):
-        """Histgram
+        """回転
         """
+        # (1) ==================================================================
+        # h, w, c = self.imgArray_512.shape
+        # rotationMat = Functions.getRotationMat(-30.0)
+        # imgArray_transformed = Functions.affine(self.imgArray_512,
+        #                                         rotationMat,
+        #                                         (h, w))
+        # self.__plotImages([self.imgArray_512, imgArray_transformed])
+        
+        # (2) ==================================================================
+        # ======================================================================
+        # 自前の実装ではうまくいかなかった
+        # ======================================================================
+        # h, w, c = self.imgArray_512.shape
+        
+        # centerX = w // 2
+        # centerY = h // 2
+        # print(f"centerX= {centerX}, centerY= {centerY}")
+        
+        # shiftMatPre = Functions.getShiftMat(-centerX, -centerY, True)
+        # rotationMat = Functions.getRotationMat(-30.0, True)
+        # shiftMatAfter = Functions.getShiftMat(centerX, centerY, True)
+        # transMat = np.dot(np.dot(shiftMatPre, rotationMat), shiftMatAfter)
+        # # transMat = np.matmul(shiftMatPre, rotationMat)
+        # # transMat = shiftMatAfter
+        # print(transMat)
+        
+        # imgArray_transformed = Functions.affine(self.imgArray_512,
+        #                                         transMat[:2],
+        #                                         (h, w))
+        
+        # ======================================================================
+        # 以下はホームページからコピペした実装
+        # ======================================================================
+        rad = - 30 / 180 * np.pi
+        h, w, c = self.imgArray_512.shape
+        tx = int((np.cos(- rad) - 1) * w // 2 - np.sin(- rad) * h // 2)
+        ty = int(np.sin(- rad) * w // 2 + (np.cos(- rad) - 1) * h // 2)
+
+        imgArray_transformed = Functions.affine(self.imgArray_512, [[np.cos(rad), - np.sin(rad), tx], [np.sin(rad), np.cos(rad), ty]], (h, w))
+
+        self.__plotImages([self.imgArray_512, imgArray_transformed])
 
     def __plotImages(self, imgs: List[np.ndarray]):
         nImgs = len(imgs)
@@ -109,7 +181,7 @@ def main():
     mainObject = Main()
 
     # mainObject.question21()
-    mainObject.question22()
+    # mainObject.question22()
     # mainObject.question23()
     # mainObject.question24()
     # mainObject.question25()
@@ -117,7 +189,7 @@ def main():
     # mainObject.question27()
     # mainObject.question28()
     # mainObject.question29()
-    # mainObject.question30()
+    mainObject.question30()
 
 
 if __name__ == "__main__":
